18a56f6f38f0134cd861860ea6b0be6b79318196
FearlessAbhay <71921632+FearlessAbhay@users.noreply.github.com>
2020-12-20 10:01:22 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,53 @@
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+  img = loadImage('dog_cat.jpeg');
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+  if(status !="")
+  {
+    for (i = 0; i < objects.length; i++)
+    {
+      document.getElementById("status").innerHTML = "Status :Object Detected";
+
+      fill("#FF0000");
+      percent = floor(objects[i].cofidence * 100);
+      text(objects[i].label + "" + percent + "%", objects[i].x +15, objects[i].y +15);
+      noFill();
+      stroke("#FF0000");
+      rect( objects[i].x, objects[i].y, objects[i].width, objects[i].hieght);
+    
+    }
+  }
+  fill("#FF0000");
+  text("Dog", 45, 75);
+  noFill();
+  stroke("#FF0000");
+  rect(30, 60, 450, 350 );
+
+  image(img, 0, 0, 640, 420);
+  fill("#FF0000");
+  text("Cat", 45, 75);
+  noFill();
+  stroke("#FF0000");
+  rect(30, 60, 450, 350 );
+
+}
+function gotResult(error, results)
+{
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
